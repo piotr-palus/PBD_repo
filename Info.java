b1d29559447e1fcba141641f88be2976f944b40d
filipjarz <32683971+filipjarz@users.noreply.github.com>
2017-11-12 23:19:54 +0100

    instrukcja dodawania postaci i ekwipunku

---
+++ Info.java
@@ -0,0 +1,24 @@
+
+public class Info {
+	/*
+	//dotyczy maina
+	//postaæ
+    GeneratorImion g = new GeneratorImion();
+    g.wypelnij; // to s³uzy do wype³nienia arraylisty sylabami, mo¿na wrzuciæ do konstruktora GereratorImion(), ale jak chcesz
+    // Teraz dzia³a wiêc nie trzeba
+    for(...){
+    // postaci ma byæ duzo
+	// do konstruktora trzeba przekazaæ te¿ iloœæ klas, ras, charakterow i inwentarzy
+    Postac p = new Postac(g.stworzImie(), ....);
+    dataBase.dodajPostac(p);
+	}
+
+	// magiczny ekwipunek
+	for(...){
+	// tych przedmiotów niech bêdzie wiêcej niz 50, tak ¿eby by³a szansa ¿e ka¿da kombinacja bêdzie
+	    MagicznyEkwipunek me = new MagicznyEkwipunek();
+        dataBase.dodajMagicznyEkwipunek(me);
+	}
+
+	 */
+}
